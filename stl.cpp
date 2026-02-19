354aa87c4f170e97bc8e828d9e5bbdaac42518af
Adarsh Yadav <officialaadi143@gmail.com>
2026-02-19 12:43:13 +0530

    Refactor stl.cpp: Comment out unused vector code and retain pair example

--- stl.cpp
+++ stl.cpp
@@ -5,13 +5,19 @@ using namespace std;
 int main()
 {
 
-    vector<vector<int>>vv(10,vector<int>(5,100));
-    for(int i=0;i<vv.size();i++)
-    {
-        for(int j=0;j<vv[i].size();j++)
-        {
-            cout<<vv[i][j]<<" ";
-            cout<<endl;
-        }
-    }
+    // vector<vector<int>>vv(10,vector<int>(5,100));
+    // for(int i=0;i<vv.size();i++)
+    // {
+    //     for(int j=0;j<vv[i].size();j++)
+    //     {
+    //         cout<<vv[i][j]<<" ";
+    //         cout<<endl;
+    //     }
+    // }
+    pair<int,int>p;
+    p = {10,200};
+    cout<<p.first;
+    cout<<",";
+    cout<<p.second;
+
 }
